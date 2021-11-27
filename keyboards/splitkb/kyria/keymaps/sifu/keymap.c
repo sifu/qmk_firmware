d7cde3503c7b7c14c0b80f8f0fc353dac3d2fd0f
Siegmund Führinger <sifu@0xx0.net>
2021-11-27 12:34:16 +0100

    home row mods

--- keyboards/splitkb/kyria/keymaps/sifu/keymap.c
+++ keyboards/splitkb/kyria/keymaps/sifu/keymap.c
@@ -35,9 +35,15 @@ enum layers {
 #define CTL_ESC  MT(MOD_LCTL, KC_ESC)
 #define CTL_QUOT MT(MOD_RCTL, KC_QUOTE)
 #define CTL_MINS MT(MOD_RCTL, KC_MINUS)
-#define ALT_ENT  MT(MOD_LALT, KC_ENT)
 
-// Note: LAlt/Enter (ALT_ENT) is not the same thing as the keyboard shortcut Alt+Enter.
+// home row mods
+#define SHFT_A    MT(MOD_LSFT, KC_A)
+#define ALT_S     MT(MOD_LALT, KC_S)
+#define CTL_D     MT(MOD_LCTL, KC_D)
+#define CTL_K     MT(MOD_LCTL, KC_K)
+#define ALT_L     MT(MOD_LALT, KC_L)
+#define SHFT_SCLN MT(MOD_LSFT, KC_SCLN)
+
 // The notation `mod/tap` denotes a key that activates the modifier `mod` when held down, and
 // produces the key `tap` when tapped (i.e. pressed and released).
 
@@ -50,6 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  Tab   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  Bksp  |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "|
+ * |        |shift | alt  | ctrl |      |      |                              |      |      | ctrl | alt  |shift |        |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |CapsLk|  |F-keys|  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
@@ -59,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_QWERTY] = LAYOUT(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                          KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
-     CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                          KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
+     CTL_ESC ,SHFT_A,  ALT_S  ,  CTL_D ,   KC_F ,   KC_G ,                                          KC_H,   KC_J , CTL_K ,  ALT_L ,SHFT_SCLN,CTL_QUOT,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC,   KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
                                 ADJUST , KC_LGUI, KC_BSPC, KC_SPC , KC_LGUI,    KC_CIRC, KC_ENT, KC_SPC , KC_RGUI, KC_APP
     ),
